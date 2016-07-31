b8311da47fa4153b6eb80cd935f0acce285ef562
Mark Harder <mark.harder899@gmail.com>
2016-07-31 18:50:09 -0400

    add synonyms to the translator
    Words like pick up and get are all translated to a related common
    word, in this case to take.

--- lib/hedge/translator.rb
+++ lib/hedge/translator.rb
@@ -5,10 +5,18 @@ module Hedge
         "q" => "quit",
         "x" => "look at",
       }
+
+      @synonyms = {
+        "trees" => "forest",
+      }
     end
 
     def translate( text )
       for k, v in @abbreviations
+        text.gsub!( /\b#{k}\b/, v )
+      end
+
+      for k, v in @synonyms
         text.gsub!( /\b#{k}\b/, v )
       end
 
