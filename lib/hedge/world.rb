b36ff235458db233fc609dec61ef3ab2cec5c2c9
Mark Harder <mark.harder899@gmail.com>
2016-07-31 19:40:12 -0400

    load rooms dynamically based on location classes
    Each individual class no longer needs to be initialized.
    The world class will now read through the location classes and
    create a room for each class that it finds.

--- lib/hedge/world.rb
+++ lib/hedge/world.rb
@@ -4,9 +4,9 @@ module Hedge
   class World
     def initialize( player )
       @player = player
-      @rooms = {
-        dias: Dias.new
-      }
+      @rooms = { }
+
+      load_rooms
     end
 
     def current_room
@@ -15,6 +15,30 @@ module Hedge
 
     def perform( command )
       current_room.perform( @player, command )
+    end
+
+    private
+    def load_rooms
+      filename = File.join( __dir__, 'locations.rb' )
+      File.foreach( filename ) do |line|
+        # get all the classnames
+        if line =~ /^\s+class\s+(.*)\s+</
+          # the classname is the paren match, stored in $1
+          classname = $1
+
+          # turn the classnames into a symbol key and an object value
+          class_symbol = classname_to_symbol( classname )
+          object = Hedge.const_get( classname ).new
+          @rooms[ class_symbol ] = object
+        end
+      end
+    end
+
+    def classname_to_symbol( classname )
+      # insert an underscore before all non-leading uppercase letters
+      # downcase the entire string
+      # LongClassName -> long_class_name
+      classname.gsub(/(.)([A-Z])/, "\1_#{$2}" ).downcase.to_sym
     end
   end
 end
